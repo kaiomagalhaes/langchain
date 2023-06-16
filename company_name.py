6149036df60b1703293d7b11f555ac2e8c9784fb
Kaio Magalhaes <me@kaiomagalhaes.com>
2023-06-16 12:43:52 -0300

    use lanchain prompts

--- company_name.py
+++ company_name.py
@@ -1,10 +1,18 @@
-from langchain.llms import OpenAI
 import os
 
+from langchain.prompts import PromptTemplate
+from langchain.llms import OpenAI
+
+
+prompt = PromptTemplate(
+    input_variables=["product"],
+    template="What is a good name for a company that makes {product}?",
+)
+
+prompt_text = prompt.format(product="colorful socks")
+
+print(prompt_text)
 
 llm = OpenAI(openai_api_key=os.environ.get('OPENAI_API_KEY'), temperature=0.9)
 
-text = "What would be a good company name for a company that makes colorful socks?"
-print(llm(text))
-
-
+print(llm(prompt_text))
